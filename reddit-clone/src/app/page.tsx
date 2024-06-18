35959422264317ccf5a2a78ce6e4742bc98038c0
Jatin Assudaney <jatin.assudaney24@gmail.com>
2024-06-19 00:22:05 +0530

    [ADD]: header + handle static caching page while using auth

--- reddit-clone/src/app/page.tsx
+++ reddit-clone/src/app/page.tsx
@@ -1,18 +1,3 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
-
-export default async function Home() {
-  return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-      <Profile />
-    </div>
-  );
+export default function Home() {
+  return <div>Home Page</div>;
 }
