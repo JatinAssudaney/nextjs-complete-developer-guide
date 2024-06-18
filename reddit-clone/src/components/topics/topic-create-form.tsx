05abe74d7b4fef3ba96f7e844da97c8111ddebd6
Jatin Assudaney <jatin.assudaney24@gmail.com>
2024-06-19 02:31:39 +0530

    [ADD]: creation of topic using zod validation

---
+++ reddit-clone/src/components/topics/topic-create-form.tsx
@@ -0,0 +1,52 @@
+"use client";
+import {
+  Input,
+  Button,
+  Textarea,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+import { useFormState } from "react-dom";
+
+export default function TopicCreateForm() {
+  const [formData, action] = useFormState(actions.createTopic, { errors: {} });
+
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={action}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Topic</h3>
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formData.errors.name}
+              errorMessage={formData.errors.name?.join(", ")}
+            />
+            <Textarea
+              name="description"
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Describe your topic"
+              isInvalid={!!formData.errors.description}
+              errorMessage={formData.errors.description?.join(", ")}
+            />
+            {formData.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formData.errors._form.join(", ")}
+              </div>
+            ) : null}
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
