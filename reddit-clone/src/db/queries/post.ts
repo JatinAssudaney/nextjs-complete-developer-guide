c1613640fa7d68b39815f097e0e135a5249e1654
Jatin Assudaney <jatin.assudaney24@gmail.com>
2024-06-23 03:27:19 +0530

    [ADD]: comment list

---
+++ reddit-clone/src/db/queries/post.ts
@@ -0,0 +1,28 @@
+import { db } from "@/db";
+
+export type PostWithData = Awaited<
+  ReturnType<typeof fetchPostsByTopicSlug>
+>[number];
+
+export function fetchPostsByTopicSlug(slug: string) {
+  return db.post.findMany({
+    where: {
+      topic: { slug },
+    },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
+
+export function fetchPosts() {
+  return db.post.findMany({
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
